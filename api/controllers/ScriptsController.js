a1316327570402c357c741b01fd83110c6119410
Neil Ni <Neil_Ni@mckinsey.com>
2015-05-17 15:56:54 -0400

    setup bower, angular, server script

---
+++ api/controllers/ScriptsController.js
@@ -0,0 +1,11 @@
+/**
+ * ScriptsController
+ *
+ * @description :: Server-side logic for managing scripts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
